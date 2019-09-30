a16a9ab1d46dca07be4dd2975e898f838dc4b56c
AlfalahAnugara <alfalah190299@gmail.com>
2019-09-30 16:44:24 +0700

    mengedit pada main activity.java

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -2,22 +2,34 @@ package id.ac.polinema.idealbodyweight;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
+import id.ac.polinema.idealbodyweight.fragments.BrocaIndexFragment;
+import id.ac.polinema.idealbodyweight.fragments.MenuFragment;
 
-	private AboutFragment aboutFragment ;
+public class MainActivity extends AppCompatActivity implements
+		MenuFragment.OnFragmentInteractionListener,
+		BrocaIndexFragment.OnFragmentInteractionListener {
 
+	// Deklarasikan atribut Fragment di sini
+	private AboutFragment aboutFragment;
+	private BrocaIndexFragment brocaIndexFragment;
+	public MenuFragment menuFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		aboutFragment = AboutFragment.newInstance("Alfalah Anugara") ;
 		super.onCreate(savedInstanceState);
+		menuFragment = new MenuFragment();
+		aboutFragment = AboutFragment.newInstance("DZIKRI");
+		brocaIndexFragment = new BrocaIndexFragment();
+
 		setContentView(R.layout.activity_main);
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, menuFragment).commit();
 	}
 
 	@Override
@@ -28,13 +40,28 @@ public class MainActivity extends AppCompatActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		// TODO: Tambahkan penanganan menu di sini
 		if (item.getItemId() == R.id.menu_about) {
 			getSupportFragmentManager().beginTransaction()
 					.replace(R.id.fragment_container, aboutFragment)
-					.addToBackStack(null)
-					.commit();
+					.addToBackStack(null).commit();
 		}
-
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onBrocaIndexButtonClicked() {
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container,brocaIndexFragment).commit();
+	}
+
+	@Override
+	public void onBodyMassIndexButtonClicked() {
+
+	}
+
+	@Override
+	public void onCalculateBrocaIndexClicked(float index) {
+
 	}
 }
